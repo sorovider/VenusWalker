1279a4824b77d780a89694687cc14e44e0873ac0
erina <erinafausett@gmail.com>
2024-02-11 00:43:23 +0900

    スマホ画面とりあえず完成

--- convertToVH.js
+++ convertToVH.js
@@ -2,8 +2,8 @@
 const args = process.argv.slice(2);
 
 // デバイスの幅と高さを更新（この場合、1440x829を使用）
-const deviceWidth = 1440;
-const deviceHeight = 829;
+const deviceWidth = 430;
+const deviceHeight = 923;
 const vw = parseFloat(args[0]);
 
 // 1vhのピクセル値を計算
@@ -15,3 +15,6 @@ const vwInPixels = deviceWidth * (vw / 100);
 const vwToVh = vwInPixels / vhInPixels;
 
 console.log(`${vwToVh.toFixed(2)}vh`);
+console.log(`max-width: ${vwToVh.toFixed(2)}vh;`);
+console.log(`min(${vw}vw, ${vwToVh.toFixed(2)}vh);`);
+console.log(`max(${vw}vw, ${vwToVh.toFixed(2)}vh);`);
