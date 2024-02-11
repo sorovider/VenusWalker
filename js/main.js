df4e3c9a2f7d2cb8ced9611be9a50026459fcbe2
erina <erinafausett@gmail.com>
2024-02-12 02:51:16 +0900

    画像ファイルパス修正

--- js/main.js
+++ js/main.js
@@ -65,7 +65,7 @@ window.addEventListener('DOMContentLoaded', () => {
         touchMove: true,
         // autoplay: true,
         // autoplaySpeed: 4000,
-        prevArrow: '<img class="slide-arrow prev-arrow" src="../images/about-work/left-arrow.png.webp" width="12" height="24" alt="左矢印">',
-        nextArrow: '<img class="slide-arrow next-arrow" src="../images/about-work/right-arrow.png.webp" width="12" height="24" alt="右矢印">',
+        prevArrow: '<img class="slide-arrow prev-arrow" src="images/about-work/left-arrow.png.webp" width="12" height="24" alt="左矢印">',
+        nextArrow: '<img class="slide-arrow next-arrow" src="images/about-work/right-arrow.png.webp" width="12" height="24" alt="右矢印">',
     });
 });
