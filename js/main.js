2cbad683c713c1b96ff5038f9db04207ea4c49b9
Hiroya In <ing@digital-sorovider.jp>
2024-02-24 14:05:36 +0900

    ハンバーガーメニューの開閉機能実装

--- js/main.js
+++ js/main.js
@@ -68,4 +68,9 @@ window.addEventListener('DOMContentLoaded', () => {
         prevArrow: '<img class="slide-arrow prev-arrow" src="images/about-work/left-arrow.png.webp" width="12" height="24" alt="左矢印">',
         nextArrow: '<img class="slide-arrow next-arrow" src="images/about-work/right-arrow.png.webp" width="12" height="24" alt="右矢印">',
     });
+
+    $('.js-hamburger-btn, .js-hamburger-item').click(() => {
+        $('#js-hamburger-menu').toggleClass('js-hamburger-menu-open')
+        $('html').toggleClass('no-scroll')
+    })
 });
